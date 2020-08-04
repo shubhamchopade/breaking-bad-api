7c504db1df2e472a8e6bada7b0d6395f82292ac4
Shubham Chopade <shubhamchopade10@gmail.com>
2020-08-05 02:03:13 +0530

    Updates

---
+++ precache-manifest.2cc3012ef2c6bd5be3f468053ccb9196.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c9ab747d5e17abd1c630b99406496b",
+    "url": "/breaking-bad-api/index.html"
+  },
+  {
+    "revision": "4a8e57a75758a1d66546",
+    "url": "/breaking-bad-api/static/css/main.a7384938.chunk.css"
+  },
+  {
+    "revision": "fc999d95d39a3e197feb",
+    "url": "/breaking-bad-api/static/js/2.f90e20d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-api/static/js/2.f90e20d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8e57a75758a1d66546",
+    "url": "/breaking-bad-api/static/js/main.5e4422ba.chunk.js"
+  },
+  {
+    "revision": "75e7bb2e1485c2ab2f65",
+    "url": "/breaking-bad-api/static/js/runtime-main.64d326c8.js"
+  },
+  {
+    "revision": "6a5bcfba04e217d91f7aec29849ae208",
+    "url": "/breaking-bad-api/static/media/tenor.6a5bcfba.gif"
+  }
+]);
